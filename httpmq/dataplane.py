44de92bbcd8ac4b0d6d79794d811d8b5c836b40c
Rhine Cliff <rhinecliff@protonmail.com>
2022-08-08 15:10:12 -0700

    Started defininf the dataplane API wrapper client, but is unable to create a push-subscription handling function which can cleanly shutdown.

--- httpmq/dataplane.py
+++ httpmq/dataplane.py
@@ -1,11 +1,96 @@
 """Wrapper object for operating the httpmq dataplane API"""
 
+# pylint: disable=too-many-arguments
+# pylint: disable=too-few-public-methods
+# pylint: disable=too-many-locals
+
+import base64
+import json
+import traceback
+from typing import Dict, List
+import urllib3
 from httpmq.core import ApiClient
 from httpmq.core.api.dataplane_api import DataplaneApi
 from httpmq.core.models import (
     GoutilsRestAPIBaseResponse,
+    DataplaneAckSeqNum,
 )
 from .common import APICallContext, HttpmqAPIError
+
+
+class RxMessageSplitter:
+    """
+    Support class for taking the text stream from the push subscription endpoint, and
+    separate that out into individual messages
+    """
+
+    def __init__(self):
+        """Constructor"""
+        self.left_over = None
+
+    def process_new_segment(self, stream_chunk: bytes) -> List[Dict[str, object]]:
+        """Given a new stream chunk, process a list of parsed DICT
+
+        :param stream_chunk: new message chunk
+        :return: list of parsed DICTs
+        """
+        if not stream_chunk:
+            return []
+
+        # Split the chunk by NL
+        lines = stream_chunk.decode("utf-8").split("\n")
+        if len(lines) == 0:
+            return []
+
+        parsed_lines = []
+        # Process each line
+        for one_line in lines:
+            to_process = one_line
+            # If there were leftovers, combine the current one with leftover
+            if self.left_over is not None:
+                to_process = self.left_over + one_line
+                self.left_over = None
+            if not to_process:
+                continue
+            try:
+                parsed = json.loads(to_process)
+                parsed_lines.append(parsed)
+            except json.decoder.JSONDecodeError:
+                # Parse failure, assume incomplete
+                self.left_over = to_process
+
+        return parsed_lines
+
+
+class ReceivedMessage:
+    """Container for a received message"""
+
+    def __init__(
+        self,
+        stream: str,
+        stream_seq: int,
+        consumer: str,
+        consumer_seq: int,
+        subject: str,
+        message: bytes,
+        request_id: str,
+    ):
+        """Constructor
+
+        :param stream: name of the stream this message is from
+        :param stream_seq: the message sequence number within this stream
+        :param consumer: name of the consumer that received the message
+        :param consumer_seq: the message sequence number for that consumer on this stream
+        :param subject: the message subject
+        :param request_id: request ID
+        """
+        self.stream = stream
+        self.stream_seq = stream_seq
+        self.consumer = consumer
+        self.consumer_seq = consumer_seq
+        self.subject = subject
+        self.message = message
+        self.request_id = request_id
 
 
 class DataAPIWrapper:
@@ -31,3 +116,179 @@ class DataAPIWrapper:
         # Not ready
         if not response.success:
             raise HttpmqAPIError.new_error(response)
+
+    def publish(self, subject: str, message: bytes, ctxt: APICallContext) -> str:
+        """Publishes a message under a subject
+
+        :param subject: the subject to publish under
+        :param message: the message to publish
+        :param ctxt: the caller context
+        :return: request ID in the response
+        """
+        # Base64 encode message
+        encoded_msg = base64.b64encode(message).decode("utf-8")
+        response: GoutilsRestAPIBaseResponse = (
+            self.client.v1_data_subject_subject_name_post(
+                subject_name=subject,
+                message=encoded_msg,
+                httpmq_request_id=ctxt.request_id,
+                async_req=False,
+                _request_timeout=ctxt.request_timeout_sec,
+                _request_auths=ctxt.auth_param,
+            )
+        )
+        # Failed
+        if not response.success:
+            raise HttpmqAPIError.new_error(response)
+        return response.request_id
+
+    def send_ack(
+        self,
+        stream: str,
+        stream_seq: int,
+        consumer: str,
+        consumer_seq: int,
+        ctxt: APICallContext,
+    ) -> str:
+        """Send a message ACK for an associated JetStream message
+
+        Each message is marked by two sequence numbers.
+          * the stream sequence number
+          * the consumer sequence number
+
+        The stream sequence number is the global ID of a message within the stream. Each
+        message receives a unique monotonically increasing stream sequence number.
+
+        The consumer sequence number tracks the messages sent to a consumer from a stream;
+        the consumer has per-stream sequence number tracking for each stream the consumer
+        listens to.
+
+        Each time a consumer receives a message from the stream (a new message, or a retransmit
+        of a previous message), that consumer's sequence number on that stream is increased.
+
+        :param stream: name of the stream this message is from
+        :param stream_seq: the message sequence number within this stream
+        :param consumer: name of the consumer that received the message
+        :param consumer_seq: the message sequence number for that consumer on this stream
+        :param ctxt: the caller context
+        :return: request ID in the response
+        """
+        response: GoutilsRestAPIBaseResponse = (
+            self.client.v1_data_stream_stream_name_consumer_consumer_name_ack_post(
+                stream_name=stream,
+                consumer_name=consumer,
+                sequence_num=DataplaneAckSeqNum(
+                    stream=stream_seq, consumer=consumer_seq
+                ),
+                httpmq_request_id=ctxt.request_id,
+                async_req=False,
+                _request_timeout=ctxt.request_timeout_sec,
+                _request_auths=ctxt.auth_param,
+            )
+        )
+        # Failed
+        if not response.success:
+            raise HttpmqAPIError.new_error(response)
+        return response.request_id
+
+    def push_subscribe(
+        self,
+        stream: str,
+        consumer: str,
+        subject_filter: str,
+        process_rx_msg_cb,
+        ctxt: APICallContext,
+        max_msg_inflight: int = None,
+        delivery_group: str = None,
+    ) -> str:
+        """Start a push subscription for a consumer on a stream
+
+        This is a blocking function which only exits when either
+          * Connection breaks
+          * Server closes the connection
+
+        :param stream: target stream
+        :param consumer: consumer name
+        :param subject_filter: subscribe for message which subject matches the filter
+        :param process_rx_msg_cb: callback function for handling a received message
+        :param ctxt: the caller context
+        :param max_msg_inflight: the max number of inflight messages if provided
+        :param delivery_group: the delivery group the consumer belongs to if the consumer
+            uses one
+        :return: request ID in the response
+        """
+
+        # Operate the urllib3 requests directly
+
+        # Target URL
+        base_url = self.client.api_client.configuration.host
+        subscribe_url = f"{base_url}/v1/data/stream/{stream}/consumer/{consumer}"
+
+        # Query parameters
+        query_params = {"subject_name": subject_filter}
+        if max_msg_inflight is not None:
+            query_params["max_msg_inflight"] = max_msg_inflight
+        if delivery_group is not None:
+            query_params["delivery_group"] = delivery_group
+
+        # Header parameters
+        header_params = {"Httpmq-Request-ID": ctxt.request_id}
+        for one_auth_param in ctxt.auth_param:
+            header_params[one_auth_param["key"]] = one_auth_param["value"]
+
+        http = self.client.api_client.rest_client.pool_manager
+
+        # Make request
+        req = http.request(
+            "GET",
+            subscribe_url,
+            fields=query_params,
+            headers=header_params,
+            preload_content=False,
+            retries=False,
+            timeout=urllib3.Timeout(total=None),
+        )
+
+        # Buffer for processing a stream of
+        stream_buffer = RxMessageSplitter()
+
+        try:
+            for chunk in req.stream():
+                parsed_lines = stream_buffer.process_new_segment(chunk)
+                # Parse the parsed lines
+                for one_parsed_line in parsed_lines:
+                    # layout the message pieces
+                    msg_consumer = one_parsed_line["consumer"]
+                    msg_consumer_seq = one_parsed_line["sequence"]["consumer"]
+                    msg_stream = one_parsed_line["stream"]
+                    msg_stream_seq = one_parsed_line["sequence"]["stream"]
+                    msg_request_id = one_parsed_line["request_id"]
+                    msg_subject = one_parsed_line["subject"]
+                    msg_body = one_parsed_line["b64_msg"]
+                    # base64 decode the message
+                    original_body = base64.b64decode(msg_body)
+                    # Package the message
+                    message = ReceivedMessage(
+                        stream=msg_stream,
+                        stream_seq=msg_stream_seq,
+                        consumer=msg_consumer,
+                        consumer_seq=msg_consumer_seq,
+                        subject=msg_subject,
+                        request_id=msg_request_id,
+                        message=original_body,
+                    )
+                    # Pass on the message
+                    process_rx_msg_cb(message)
+        except Exception as err:
+            raise HttpmqAPIError(
+                request_id=ctxt.request_id,
+                status_code=req.status,
+                message="push-subscription read error",
+                detail="".join(
+                    traceback.format_exception(type(err), err, err.__traceback__)
+                ),
+            ) from err
+        finally:
+            req.release_conn()
+
+        return ctxt.request_id
--- test/test_dataplane.py
+++ test/test_dataplane.py
@@ -2,15 +2,21 @@
 
 
 import unittest
+import uuid
 import urllib3
 from httpmq import CoreConfiguration, CoreApiClient
+import httpmq
 from httpmq.common import APICallContext
-from httpmq.dataplane import DataAPIWrapper
+from httpmq.core.models import (
+    ManagementJSStreamParam,
+    ManagementJetStreamConsumerParam,
+)
+from httpmq.dataplane import DataAPIWrapper, RxMessageSplitter
 from httpmq.management import MgmtAPIWrapper
 from . import get_unittest_httpmq_data_api_url, get_unittest_httpmq_mgmt_api_url
 
 
-class TestManagementPlane(unittest.TestCase):
+class TestDataPlane(unittest.TestCase):
     """Test bench for httpmq.dataplane"""
 
     def setUp(self):
@@ -24,8 +30,47 @@ class TestManagementPlane(unittest.TestCase):
         self.core_data_client = CoreApiClient(configuration=self.core_data_cfg)
         self.data_client = DataAPIWrapper(api_client=self.core_data_client)
 
+    def test_message_splitter(self):
+        """Basic sanity check RxMessageSplitter"""
+
+        uut = RxMessageSplitter()
+
+        test_cases = [
+            {
+                "input": b'{"hello":',
+                "expect": [],
+            },
+            {
+                "input": b'"world"}\n{"a":12,"c',
+                "expect": [{"hello": "world"}],
+            },
+            {
+                "input": b'":{"b":1.31,"d":"hhase"}}\n{"hello":"wow"}',
+                "expect": [{"a": 12, "c": {"b": 1.31, "d": "hhase"}}, {"hello": "wow"}],
+            },
+            {
+                "input": b'{"a":"a8931",',
+                "expect": [],
+            },
+            {
+                "input": None,
+                "expect": [],
+            },
+            {
+                "input": "",
+                "expect": [],
+            },
+            {
+                "input": b'"b":-0.0193}',
+                "expect": [{"a": "a8931", "b": -0.0193}],
+            },
+        ]
+        for one_case in test_cases:
+            parsed_list = uut.process_new_segment(one_case["input"])
+            self.assertListEqual(one_case["expect"], parsed_list)
+
     def test_basic_sanity(self):
-        """Basic sanity check of management API client"""
+        """Basic sanity check of dataplane API client"""
 
         self.data_client.ready(ctxt=APICallContext())
 
@@ -36,3 +81,121 @@ class TestManagementPlane(unittest.TestCase):
         another_mgmt_client = DataAPIWrapper(api_client=another_client)
         with self.assertRaises(urllib3.exceptions.MaxRetryError):
             another_mgmt_client.ready(ctxt=APICallContext())
+
+    def test_push_subscribe(self):
+        """Basic functionality test of push subscription"""
+
+        self.mgmt_client.ready(ctxt=APICallContext())
+        self.data_client.ready(ctxt=APICallContext())
+
+        # Case 0: create a stream and consumer
+        stream_0 = str(uuid.uuid4())
+        subject_base = str(uuid.uuid4())
+        subjects_0 = [f"{subject_base}.a", f"{subject_base}.b"]
+        subject_wildcard = f"{subject_base}.*"
+        stream_0_param = ManagementJSStreamParam(name=stream_0, subjects=subjects_0)
+        ctxt = APICallContext()
+        self.assertEqual(
+            self.mgmt_client.create_stream(params=stream_0_param, ctxt=ctxt),
+            ctxt.request_id,
+        )
+        ctxt = APICallContext()
+        stream_0_rb, rid = self.mgmt_client.get_stream(stream=stream_0, ctxt=ctxt)
+        self.assertEqual(rid, ctxt.request_id)
+        self.assertEqual(stream_0_rb.config.name, stream_0)
+        self.assertListEqual(stream_0_rb.config.subjects, subjects_0)
+        consumer_0 = str(uuid.uuid4())
+        consumer_param = ManagementJetStreamConsumerParam(
+            name=consumer_0,
+            mode="push",
+            max_inflight=1,
+            filter_subject=subjects_0[0],
+        )
+        ctxt = APICallContext()
+        self.assertEqual(
+            self.mgmt_client.create_consumer_for_stream(
+                stream=stream_0, params=consumer_param, ctxt=ctxt
+            ),
+            ctxt.request_id,
+        )
+        consumer_1 = str(uuid.uuid4())
+        consumer_param = ManagementJetStreamConsumerParam(
+            name=consumer_1,
+            mode="push",
+            max_inflight=1,
+            filter_subject=subjects_0[1],
+        )
+        ctxt = APICallContext()
+        self.assertEqual(
+            self.mgmt_client.create_consumer_for_stream(
+                stream=stream_0, params=consumer_param, ctxt=ctxt
+            ),
+            ctxt.request_id,
+        )
+        consumer_2 = str(uuid.uuid4())
+        consumer_param = ManagementJetStreamConsumerParam(
+            name=consumer_2,
+            mode="push",
+            max_inflight=1,
+            filter_subject=subject_wildcard,
+        )
+        ctxt = APICallContext()
+        self.assertEqual(
+            self.mgmt_client.create_consumer_for_stream(
+                stream=stream_0, params=consumer_param, ctxt=ctxt
+            ),
+            ctxt.request_id,
+        )
+
+        # There is no clean way to shutdown these threads
+
+        # # Case 1: subscribe for messages
+        # c0_rx0_msg_queue = queue.Queue()
+        # c1_rx0_msg_queue = queue.Queue()
+        # c2_rx0_msg_queue = queue.Queue()
+
+        # def write_to_c0_rx0_msg_queue(msg: ReceivedMessage):
+        #     c0_rx0_msg_queue.put(msg)
+
+        # def write_to_c1_rx0_msg_queue(msg: ReceivedMessage):
+        #     c1_rx0_msg_queue.put(msg)
+
+        # def write_to_c2_rx0_msg_queue(msg: ReceivedMessage):
+        #     c2_rx0_msg_queue.put(msg)
+
+        # # Helper thread to run the RX loop
+        # def read_rx_msg(
+        #     consumer: str, subject_filter: str, rx_msg_log_cb, ctxt: APICallContext
+        # ):
+        #     with self.assertRaises(Exception):
+        #         self.data_client.push_subscribe(
+        #             stream=stream_0,
+        #             consumer=consumer,
+        #             subject_filter=subject_filter,
+        #             process_rx_msg_cb=rx_msg_log_cb,
+        #             ctxt=ctxt,
+        #         )
+
+        # rx0_ctxt = APICallContext()
+        # rx1_ctxt = APICallContext()
+        # rx2_ctxt = APICallContext()
+        # threading.Thread(
+        #     target=read_rx_msg,
+        #     args=(consumer_0, subjects_0[0], write_to_c0_rx0_msg_queue, rx0_ctxt),
+        # ).start()
+        # threading.Thread(
+        #     target=read_rx_msg,
+        #     args=(consumer_1, subjects_0[1], write_to_c1_rx0_msg_queue, rx1_ctxt),
+        # ).start()
+        # threading.Thread(
+        #     target=read_rx_msg,
+        #     args=(consumer_2, subject_wildcard, write_to_c2_rx0_msg_queue, rx2_ctxt),
+        # ).start()
+
+        # Delete the stream
+        ctxt = APICallContext()
+        self.assertEqual(
+            self.mgmt_client.delete_stream(stream=stream_0, ctxt=ctxt), ctxt.request_id
+        )
+        with self.assertRaises(httpmq.core.exceptions.ServiceException):
+            self.mgmt_client.get_stream(stream=stream_0, ctxt=APICallContext())
